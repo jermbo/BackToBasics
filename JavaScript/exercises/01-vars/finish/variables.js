43ecae866a5a35851194b1c53491ea0f07c6c7bc
jermbo <jermbo@cosmicstrawberry.com>
2019-11-18 13:51:54 -0500

    adding variables exercises

---
+++ JavaScript/exercises/01-vars/finish/variables.js
@@ -0,0 +1,43 @@
+// -------------------------------
+// Variables Here. Variables There
+// -------------------------------
+
+// -------------------------------------------------------------------
+// You are going to be creating a series of variables.
+// You need to name the variables something that makes sense
+// and represents the values it's going to hold.
+//
+// For example :
+// var currentDate = new Date();
+// This hold the current time stamp at the time of parsing
+//
+// Once you create a variable for all seven items above, console log them out to display in broswer
+// console.log( value );
+// -------------------------------------------------------------------
+
+// 1. Change all `vars` to `let` or `const`
+// 2. Replace all ___ with meaningful variable names
+
+// A nice message to the user
+let greeting = "Welcome to JavaScript!";
+// the year WW2 ended
+const victoryDay = "1945-09-02";
+// Futurama Locations
+const locations = ["New New York", "Old New York", "Planet Express Building", "Head Museum", "Parallel Universe"];
+// The fact that JavaScript Rocks!!!
+let jsRocks = true;
+// The user is not logged in
+let isUserLoggedIn = false;
+// Character Information
+const characterInfo = { firstName: "Philip", lastName: "Fry", movie: "Futurama" };
+// First few digits of PI
+const pi = 3.141592653;
+
+// Log the variables here
+console.log(greeting);
+console.log(victoryDay);
+console.log(locations);
+console.log(jsRocks);
+console.log(isUserLoggedIn);
+console.log(characterInfo);
+console.log(pi);
---
+++ JavaScript/exercises/01-vars/start/variables.js
@@ -0,0 +1,37 @@
+// -------------------------------
+// Variables Here. Variables There
+// -------------------------------
+
+// -------------------------------------------------------------------
+// You are going to be creating a series of variables.
+// You need to name the variables something that makes sense
+// and represents the values it's going to hold.
+//
+// For example :
+// var currentDate = new Date();
+// This hold the current time stamp at the time of parsing
+//
+// Once you create a variable for all seven items above, console log them out to display in broswer
+// console.log( value );
+// -------------------------------------------------------------------
+
+// 1. Change all `vars` to `let` or `const`
+// 2. Replace all ___ with meaningful variable names
+
+// A nice message to the user
+var greeting = "Welcome to JavaScript!";
+// the year WW2 ended
+var ___ = "1945-09-02";
+// Futurama Locations
+var ___ = ["New New York", "Old New York", "Planet Express Building", "Head Museum", "Parallel Universe"];
+// The fact that JavaScript Rocks!!!
+var ___ = true;
+// The user is not logged in
+var ___ = false;
+// Character Information
+var ___ = { firstName: "Philip", lastName: "Fry", movie: "Futurama" };
+// First few digits of PI
+var ___ = 3.141592653;
+
+// Log the variables here
+console.log(greeting);
